10424deefa5d13929e070b7c06e51fb411fdbb09
RojerGS <rodrigogiraoserrao@gmail.com>
2020-02-18 12:44:49 +0000

    Adicionar código para cripto com cifras de subst.

---
+++ crypto-substitution-ciphers/caesar.py
@@ -0,0 +1,48 @@
+ABC = "abcdefghijklmnopqrstuvwxyz"
+
+def encrypt(key, msg):
+    """Encrypt a message with the given key with the Caesar cipher."""
+
+    msg = msg.lower()
+    encrypted = ""
+    
+    for char in msg:
+        if char in ABC:
+            idx = ABC.index(char)
+            new_idx = (idx + key) % len(ABC)
+            encrypted += ABC[new_idx]
+        else:
+            encrypted += char
+
+    return encrypted
+
+def decrypt_dup(key, msg):
+    """Decrypt a message that was encrypted with the given key."""
+
+    msg = msg.lower()
+    decrypted = ""
+
+    for char in msg:
+        if char in ABC:
+            idx = ABC.index(char)
+            new_idx = (idx - key) % len(ABC)
+            decrypted += ABC[new_idx]
+        else:
+            decrypted += char
+
+    return decrypted
+
+def decrypt(key, msg):
+    """Decrypt a message that was encrypted with the given key."""
+
+    return encrypt(len(ABC) - key, msg)
+
+def brute_force(msg):
+    """Decrypt a message by brute-force."""
+
+    padding = len(str(len(ABC) - 1))
+
+    for key, char in enumerate(ABC):
+        decrypted = decrypt(key, msg)
+        
+        print(f"{str(key).ljust(padding)} - {char} - {decrypted}")
